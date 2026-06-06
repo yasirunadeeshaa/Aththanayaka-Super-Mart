c4a6bf2bac3822201957860416f7e675f4fc6817
A . Yasiru Nadeesha Aththanayaka <a.y.nadeeshaaththanayaka@gmail.com>
2026-06-06 23:15:39 +0530

    add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind Working!
-      </h1>
-    </div>
-  );
-}
+import Navbar from './pages/Navigation'
+import Hero from './pages/HeroSection'
+import Footer from './pages/Footer'
+import Delivary from './pages/DelivaryPage'
+import Products from './pages/Products'
+import AboutUs from './pages/AboutUs'
+import Packaging from './pages/PackagingPage'
+import Wholesale from './pages/WholesalePage'
 
-export default App
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Products />
+      <Packaging />
+      <AboutUs />
+      <Delivary />
+      <Wholesale />
+      <Footer />
+    </>
+  )
+}
