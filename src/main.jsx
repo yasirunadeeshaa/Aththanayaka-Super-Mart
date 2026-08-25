fe0dc1afc1c35efc12cc417025bd8634f4c730f5
A . Yasiru Nadeesha Aththanayaka <a.y.nadeeshaaththanayaka@gmail.com>
2026-08-26 03:41:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 import './index.css'
 import { LanguageProvider } from "./LanguageContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>,
 )
